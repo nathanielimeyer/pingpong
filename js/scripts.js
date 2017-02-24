1c1e25c69d65891a624298217f12d41ee57a2ef2
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-02-24 10:01:36 -0800

    Refactor to prepare for negatives

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,23 @@ var talkBack = [];
 var echoFunction = function(number) {
   talkBack = [];
   for (i = 1; i <= number; i++) {
-    if (i % 15 === 0) {
-      talkBack.push("ping-pong");
-    }
-    else if (i % 5 === 0) {
-      talkBack.push("pong");
-    }
-    else if (i % 3 === 0) {
-      talkBack.push("ping");
-    } else {
-      talkBack.push(i);
-    }
+    talkBackPusher(i);
   };
   return talkBack;
+};
+
+var talkBackPusher = function(number) {
+  if (i % 15 === 0) {
+    talkBack.push("ping-pong");
+  }
+  else if (i % 5 === 0) {
+    talkBack.push("pong");
+  }
+  else if (i % 3 === 0) {
+    talkBack.push("ping");
+  } else {
+    talkBack.push(i);
+  }
 };
 
 // Front end logic
